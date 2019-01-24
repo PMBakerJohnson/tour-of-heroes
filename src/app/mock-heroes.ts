a91c42ffbdb10a3fb53c563ac59c8a535894e7ee
Peyton <p.bakerjohnson@gmail.com>
2019-01-24 05:37:48 -0600

    Angular tour of heroes test project, initial commit.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Superman'},
+  { id: 12, name: 'Batman'},
+  { id: 13, name: 'Wonder Woman'},
+  { id: 14, name: 'Jon Stewart'},
+  { id: 15, name: 'Khamala Khan'},
+  { id: 16, name: 'Miles Morales'},
+  { id: 17, name: 'Steve Rogers'},
+  { id: 18, name: 'Nicolas Cage'},
+  { id: 19, name: 'Nuggins'},
+  { id: 20, name: "I'm done"}
+]
